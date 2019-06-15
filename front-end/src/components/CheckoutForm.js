be1539131d8d39a0b3f50264cf186cacfe66c780
edreeseg <edward.reeseg@gmail.com>
2019-06-15 00:31:58 -0400

    Basic controlled inputs for testing

--- front-end/src/components/CheckoutForm.js
+++ front-end/src/components/CheckoutForm.js
@@ -1,12 +1,12 @@
-import React, { PureComponent } from "react";
-import { injectStripe } from "react-stripe-elements";
+import React, { PureComponent, useState } from 'react';
+import { injectStripe } from 'react-stripe-elements';
 
 class CheckoutForm extends PureComponent {
   handleSubmit = async ev => {
     ev.preventDefault();
     try {
-      const method = await this.props.stripe.createPaymentMethod("card", {
-        billing_details: { name: "Edward Reeseg" }
+      const method = await this.props.stripe.createPaymentMethod('card', {
+        billing_details: { name: 'Edward Reeseg' },
       });
     } catch (err) {
       console.log(err);
@@ -21,6 +21,64 @@ class CheckoutForm extends PureComponent {
       </form>
     );
   }
+}
+
+function AddressSection(props) {
+  const [streetAddress, setStreetAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [stateAbbr, setStateAbbr] = useState('');
+  const [zip, setZip] = useState('');
+  return (
+    <>
+      <input
+        placeholder="Street Address"
+        onChange={ev => setStreetAddress(ev.target.value)}
+        value={streetAddress}
+      />
+      <input
+        placeholder="City"
+        onChange={ev => setCity(ev.target.value)}
+        value={city}
+      />
+      <input
+        placeholder="State Abbreviation"
+        onChange={ev => setStateAbbr(ev.target.value)}
+        value={stateAbbr}
+        maxLength="2"
+      />
+      <input
+        placeholder="Zip Code"
+        onChange={ev => setZip(ev.target.value)}
+        value={zip}
+      />
+    </>
+  );
+}
+
+function CardSection(props) {
+  const [cardNum, setCardNum] = useState('');
+  const [exp, setExp] = useState('');
+  const [cvv, setCvv] = useState('');
+
+  return (
+    <>
+      <input
+        placeholder="Card Number"
+        onChange={ev => setCardNum(ev.target.value)}
+        value={cardNum}
+      />
+      <input
+        placeholder="Expiration Date"
+        onChange={ev => setExp(ev.target.value)}
+        value={exp}
+      />
+      <input
+        placeholder="CVV"
+        onChange={ev => setCvv(ev.target.value)}
+        value={cvv}
+      />
+    </>
+  );
 }
 
 export default CheckoutForm;
